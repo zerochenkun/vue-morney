02f07fe8a84fa1a3116a0538600808b18c9f97f5
chenuvi <chenuvi@outlook.com>
2020-03-11 23:50:06 +0800

    feat:tags 组件完成

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -13,7 +13,7 @@
             <button @click="inputContent">7</button>
             <button @click="inputContent">8</button>
             <button @click="inputContent">9</button>
-            <button class="ok" @click="ok">OK</button>
+            <button >OK</button>
             <button class="zero" @click="inputContent">0</button>
             <button @click="inputContent">.</button>
         </div>
@@ -52,7 +52,6 @@
         clear(){
             this.output = '0'
         }
-        // ok(){}
     }
 </script>
 
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,13 +1,13 @@
 <template>
     <Layout class-prefix="layout">
+        <Tags :data-source.sync="tags"/>
         <Notes/>
-        <Tags/>
-        <Types/>
+        <Types/>    
         <NumberPad/>
     </Layout>
 </template>
 
-<script lang="ts">
+<script>
     import NumberPad from '@/components/Money/NumberPad.vue';
     import Types from '@/components/Money/Types.vue';
     import Notes from '@/components/Money/Notes.vue';
@@ -16,6 +16,11 @@
     export default({
         name:'Money',
         components:{Tags, Notes, Types, NumberPad},
+        data(){
+            return {
+                tags:['衣','食','住','行']
+            }
+        }
     })
 </script>   
 
