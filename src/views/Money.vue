796f026616f8ff0dba78e2081bd138e64ae0f4d7
chenuvi <chenuvi@outlook.com>
2020-03-22 21:18:35 +0800

    feat:使用å全局 tagListï“

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -18,7 +18,7 @@
     import Types from '@/components/Money/Types.vue';
     import FormItem from '@/components/Money/FormItem.vue';
     import Tags from '@/components/Money/Tags.vue';  
-    import {Component} from 'vue-property-decorator';
+    import {Component,Watch} from 'vue-property-decorator';
     import recordListModel from '@/models/recordListModel';
     import tagListModel from '@/models/tagListModel'
 
@@ -30,7 +30,7 @@
         components:{Tags,FormItem, Types, NumberPad},
     })
     export default class Money extends Vue {
-        tags = tagList;
+        tags = window.tagList;
         recordList: RecordItem[] = recordList;
         record: RecordItem = {
             tags:[],notes:'',type:'-',amount:0
@@ -46,9 +46,11 @@
             this.record.amount = parseFloat(value)
         }
         saveRecord(){
-            const record2: RecordItem = recordListModel.clone(this.record);
-            record2.createdAt = new Date();
-            this.recordList.push(record2)
+            recordListModel.create(this.record)
+        }
+        @Watch('recordList')
+        onRecordListChange(){
+            recordListModel.save();
         }
     }
 </script>   
